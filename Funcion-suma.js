e8da45441c33a3b0beab4913f9f6ac0a2831e145
Fr4nqu1t01 <villalbafranco482@gmail.com>
2025-07-14 11:48:32 -0300

    Elimine ejercicio que estaba en la rama MAIN

--- Funcion-suma.js
+++
@@ -1,9 +0,0 @@
-let x = 5;
-let y = 12;
-
-function suma(x, y) {
-  return x + y;
-}
-// lo que esta entre parentesis (), EJECUTAN la accion
-let respuesta = suma(x, y);
-console.log(`la respuesta de ${x} + ${y} es igual a:`, respuesta);
