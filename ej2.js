6aa30ebecd027885c55923691b1346ab95537f71
Christian Sanchez <antulabi@gmail.com>
2025-07-14 20:48:55 -0300

    Add funcion flecha

--- ej2.js
+++ ej2.js
@@ -2,7 +2,13 @@
 let datos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let inverso = [];
 console.log(`Datos originales: ${datos}`);
-for (let i = 0; i < datos.length; i++) {
+for (let i = 1; i < datos.length; i++) {
   inverso[i] = datos[datos.length - 1 - i];
 }
 console.log(`Datos invertidos: ${inverso}`);
+
+
+const restar = (a, b) => {
+  return a - b;
+};
+console.log(restar(10, 4));  // 6
